0143997312df53c87306345a7a490af4df2019a8
tawaunl <tawaunl@gmail.com>
2025-08-18 10:14:48 -0700

    CollecTriFulldataset

--- AnalysisScripts/RunFullCollecTri.sh
+++ AnalysisScripts/RunFullCollecTri.sh
@@ -3,10 +3,11 @@
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
 #SBATCH --mail-user=lucast3@gene.com     # Where to send mail
 #SBATCH --ntasks=1                # Run on a single CPU
-#SBATCH --partition="defq"         #Run on Himem or defq partition
-#SBATCH --mem=120gb                     # Job memory request
+#SBATCH --partition="himem"         #Run on Himem or defq partition
+#SBATCH --mem=350gb                     # Job memory request
 #SBATCH --cpus-per-task=7            # Number of CPU cores per task
 #SBATCH --output=/gstore/scratch/u/lucast3/fibroMultiome/CollectTriOut_Full.json   # Standard output and error log
+#SBATCH --qos=long
 
 pwd; hostname; date
 
